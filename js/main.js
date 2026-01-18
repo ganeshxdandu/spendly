ad7767010ea7de2122d40dadd65a8a76c6cce5b2
Ganesh Dandu <ganeshdandu.co@gmail.com>
2026-01-18 10:16:03 +0530

    Added Delete Expense Feature

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ const errorContainer = document.querySelector(".error-container");
 const startLabel = document.querySelector(".start");
 const endLabel = document.querySelector(".end");
 const totalLabel = document.querySelector(".total");
+const pageNumberContainer = document.querySelector(".page-numbers");
 
 const inputValues = {
   title: document.querySelector("#title"),
@@ -22,14 +23,20 @@ const inputValues = {
 
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
+const MAX_ROWS = 10;
+let currentPage = 1;
+
 function renderApp() {
   if (expenses.length === 0) {
     emptyState.style.display = "flex";
     table.style.display = "none";
+    pageNumberContainer.innerHTML = "";
   } else {
     emptyState.style.display = "none";
     table.style.display = "block";
-    renderTable(expenses);
+
+    renderTable(expenses, currentPage);
+    renderPagination(expenses);
   }
 }
 
@@ -49,6 +56,7 @@ function openModal(target) {
 
 function getExpense() {
   return {
+    id: crypto.randomUUID(),
     title: inputValues.title.value,
     category: inputValues.category.value,
     amount: inputValues.amount.value,
@@ -71,33 +79,82 @@ function closeModal() {
   addExpenseModal.style.right = "-300px";
 }
 
-function renderTable(expenses, pageNumber = 2) {
-  const MAX_ROWS = 10;
-
+function renderTable(expenses, pageNumber = 1) {
   tableBody.innerHTML = "";
-  for (
-    let i = (pageNumber - 1) * MAX_ROWS;
-    i < pageNumber * MAX_ROWS && i < expenses.length;
-    i++
-  ) {
-    tableBody.innerHTML += `<tr>
-                            <td>${expenses[i].date}</td>
-                            <td>${expenses[i].title}</td>
-                            <td>${expenses[i].category}</td>
-                            <td>${expenses[i].amount}</td>
-                            <td>
-                                <i class="ph ph-pencil-simple"></i>
-                                <i class="ph ph-trash"></i>
-                            </td>
-                        </tr>`;
+
+  const start = (pageNumber - 1) * MAX_ROWS;
+  const end = pageNumber * MAX_ROWS;
+
+  for (let i = start; i < end && i < expenses.length; i++) {
+    tableBody.innerHTML += `
+      <tr data-id = ${i}>
+        <td>${expenses[i].date}</td>
+        <td>${expenses[i].title}</td>
+        <td>${expenses[i].category}</td>
+        <td>${expenses[i].amount}</td>
+        <td>
+          <div class='actions'>
+          <i class="ph ph-pencil-simple edit-expense"></i>
+          <i class="ph ph-trash delete-expense"></i>
+          </div>
+        </td>
+      </tr>
+    `;
   }
 
-  startLabel.textContent = (pageNumber - 1) * MAX_ROWS + 1;
-  endLabel.textContent =
-    pageNumber * MAX_ROWS > expenses.length
-      ? expenses.length
-      : pageNumber * MAX_ROWS;
+  const actions = document.querySelectorAll(".actions");
+  actions.forEach((element) => {
+    element.addEventListener("click", function (evt) {
+    if (evt.target.classList.contains("delete-expense")) {
+      let id = event.target.closest("tr").dataset.id;
+      expenses.splice(id, id + 1);
+      localStorage.setItem('expenses', JSON.stringify(expenses));
+      renderApp();
+      console.log(id);
+      
+    }
+  });
+  })
+
+  startLabel.textContent = expenses.length === 0 ? 0 : start + 1;
+  endLabel.textContent = Math.min(end, expenses.length);
   totalLabel.textContent = expenses.length;
+}
+
+function renderPagination(expenses) {
+  pageNumberContainer.innerHTML = "";
+
+  const MAX_PAGES = 5;
+  const totalPages = Math.ceil(expenses.length / MAX_ROWS);
+  let currentSet = Math.floor(currentPage / MAX_PAGES) + 1;
+
+  let pageStart = (currentSet - 1) * MAX_PAGES + 1;
+  let endPage =
+    currentSet * MAX_PAGES < totalPages ? currentSet * MAX_PAGES : totalPages;
+
+  while (pageStart <= endPage) {
+    const btn = document.createElement("button");
+    btn.textContent = pageStart;
+
+    if (pageStart === currentPage) {
+      btn.classList.add("active");
+    }
+
+    btn.addEventListener("click", (evt) => {
+      evt.stopPropagation();
+      currentPage = Number(evt.target.textContent);
+      renderApp();
+    });
+
+    pageNumberContainer.appendChild(btn);
+    pageStart++;
+  }
+
+  if (totalPages > 5) {
+    const btn = document.createElement("button");
+    btn.textContent = "...";
+    pageNumberContainer.appendChild(btn);
+  }
 }
 
 addExpenseBtn.addEventListener("click", (event) => {
@@ -118,6 +175,7 @@ submitExpenseBtn.addEventListener("click", () => {
     let expense = getExpense();
     expenses.push(expense);
     localStorage.setItem("expenses", JSON.stringify(expenses));
+    currentPage = Math.ceil(expenses.length / MAX_ROWS);
     renderApp();
     document.querySelector("#expense-form").reset();
     closeModal();
